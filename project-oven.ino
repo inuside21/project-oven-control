c21a6493533d4d5a1bf6c893ecc3aa2efd727211
inuside21 <martinmexico3@gmail.com>
2023-10-11 00:31:35 +0800

    updated

--- project-oven.ino
+++ project-oven.ino
@@ -10,32 +10,44 @@
 const char* ssid = "OVENWIFI";
 const char* password = "12345678";
 String serverName = "http://martorenzo.click/project/oven/server/";  // include "/"
-String deviceName = "1";                                           
+String deviceName = "1";                 
+int wifiConnected = 0;                          
+int wifiStatus = 2; // 0 - manual
+                    // 1 - wifi
+                    // 2 - choose
 
 // DISPLAY
 LiquidCrystal_I2C lcd(0x27, 20, 4); 
 long nextClear = 0;
 
 // Current
-Adafruit_ADS1115 ads;
+//Adafruit_ADS1115 ads;
 const float FACTOR = 3.2; //20A/1V from teh CT
 const float multiplier = 0.00005;
-float sCurrent;
+float sCurrent = 0;
 
 // TEMP / HUMI
-#define DHTPIN 25            //what pin we're connected to
+#define DHTPIN 12            //what pin we're connected to
 #define DHTTYPE DHT21       //DHT 21  (AM2301)
 DHT dht(DHTPIN, DHTTYPE);   //Initialize DHT sensor for normal 16mhz Arduino
-float sHumi;  //Stores humidity value
-float sTemp; //Stores temperature value
+float sHumi = 0;  //Stores humidity value
+float sTemp = 0; //Stores temperature value
 
 // Control - Out
 int pinLock = 14;
 int pinFan = 32;
 int pinHeater = 33;
-int pinLedGreen = 25;
-int pinLedRed = 26;
-int pinLedOrange = 27;
+int pinLedGreen = 27;
+int pinLedRed = 25;
+int pinLedOrange = 26;
+
+// Button
+int isSetup = 0;  // 0 - main display
+                  // 1 - timer setting
+int btnUp = 15;
+int btnDown = 2;
+int btnOK = 4;
+int btnLock = 16;
 
 // Data
 String dStatus = "0";
@@ -45,10 +57,16 @@ String dHumi = "0";
 String dTemp = "0";
 String dTimer = "00:00:00";
 String dTimerMain = "00:00:00";
+int manualTimer = -1; // -1   idle
+                      // 0    complete
+                      // >0   running
 
 // debounce
 unsigned long lastTime = 0;
+unsigned long lastTimeBtn = 0;
+unsigned long lastTimeManualRunning = 0;
 unsigned long timerDelay = 1000;
+unsigned long timerDelay2 = 500;
 
 
 // ============================
@@ -59,28 +77,17 @@ void setup()
   // Serial
   Serial.begin(9600);
 
-  // init wifi
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-
   // init lcd
   lcd.init(); //initialize the lcd
   lcd.backlight(); //open the backlight 
   lcd.clear();
 
   // init current
-  ads.setGain(GAIN_FOUR);      // +/- 1.024V 1bit = 0.5mV
-  ads.begin();
+  //ads.setGain(GAIN_FOUR);      // +/- 1.024V 1bit = 0.5mV
+  //ads.begin();
 
   // init temp
-  dht.begin();
+  //dht.begin();
 
   // init Control - Out
   pinMode(pinLock, OUTPUT);
@@ -95,6 +102,12 @@ void setup()
   digitalWrite(pinLedGreen, LOW);
   digitalWrite(pinLedRed, LOW);
   digitalWrite(pinLedOrange, LOW);
+  
+  // init Button
+  pinMode(btnUp, INPUT_PULLUP);
+  pinMode(btnDown, INPUT_PULLUP);
+  pinMode(btnOK, INPUT_PULLUP);
+  pinMode(btnLock, INPUT_PULLUP);
 }
 
 
@@ -104,72 +117,199 @@ void setup()
 // ============================
 void loop() 
 {
-  if ((millis() - lastTime) > timerDelay) 
+  // Get Current
+  //float voltage = ads.readADC_Differential_0_1() * multiplier; // ads.readADC_Differential_0_1 readADC_Differential_2_3
+  sCurrent = 0; //voltage * FACTOR;
+
+  // Get Temp
+  sHumi = 99;//dht.readHumidity();
+  sTemp= 99; //dht.readTemperature();
+
+  // Button
+  int btnUpVal = digitalRead(btnUp);
+  int btnDownVal = digitalRead(btnDown);
+  int btnOKVal = digitalRead(btnOK);
+  int btnLockVal = digitalRead(btnLock);
+
+  // AUTO
+  if (wifiStatus == 1)
   {
-    if(WiFi.status()== WL_CONNECTED)
+    //
+    if (wifiConnected == 0)
     {
-      // Get Current
-      float voltage = ads.readADC_Differential_0_1() * multiplier; // ads.readADC_Differential_0_1 readADC_Differential_2_3
-      sCurrent = voltage * FACTOR;
+      ConnectWifi();
+    }
 
-      // Get Temp
-      sHumi = dht.readHumidity();
-      sTemp= dht.readTemperature();
-
-      // Set Data
-      RequestSetData();
-      RequestSetTimer();
-
-      // Get Data
-      RequestGetLock();
-      RequestGetCurrent();
-      RequestGetHumi();
-      RequestGetTemp();
-      RequestGetTimer();
-      RequestGetTimerMain();
-
-      // Display - Idle
-      if (dStatus == "0")
+    //
+    if ((millis() - lastTime) > timerDelay) 
+    {
+      // Connected
+      if(WiFi.status()== WL_CONNECTED)
       {
-        lcd.setCursor(0, 0);        
-        lcd.print("                    "); 
-        lcd.setCursor(0, 1);        
-        lcd.print("       STATUS       "); 
-        lcd.setCursor(0, 2);        
-        lcd.print("        IDLE        "); 
-        lcd.setCursor(0, 3);        
-        lcd.print("                    "); 
+        // Set Data
+        RequestSetData();
+        RequestSetTimer();
 
-        // Out
-        digitalWrite(pinFan, LOW);
-        digitalWrite(pinHeater, LOW);
-        digitalWrite(pinLedGreen, LOW);
-        digitalWrite(pinLedRed, LOW);
-        digitalWrite(pinLedOrange, HIGH);
+        // Get Data
+        RequestGetStatus();
+        RequestGetLock();
+        RequestGetCurrent();
+        RequestGetHumi();
+        RequestGetTemp();
+        RequestGetTimer();
+        RequestGetTimerMain();
+
+        // Display - Idle
+        if (dStatus == "0")
+        {
+          lcd.setCursor(0, 0);        
+          lcd.print("       STATUS       "); 
+          lcd.setCursor(0, 1);        
+          lcd.print("        IDLE        "); 
+          lcd.setCursor(0, 2);        
+          lcd.print("                    "); 
+          lcd.setCursor(0, 3);        
+          lcd.print("     Via  Phone     "); 
+
+          // Out
+          digitalWrite(pinFan, LOW);
+          digitalWrite(pinHeater, LOW);
+          digitalWrite(pinLedGreen, LOW);
+          digitalWrite(pinLedRed, LOW);
+          digitalWrite(pinLedOrange, HIGH);
+        }
+
+        // Display - In use
+        if (dStatus == "1")
+        {
+          lcd.setCursor(0, 0);        
+          lcd.print("   " + dTemp + "c  " + dHumi + "%   "); // 00.00
+          lcd.setCursor(0, 1);        
+          lcd.print(" " + dTimer + "--" + dTimerMain + " "); 
+          lcd.setCursor(0, 2);        
+          lcd.print("       STATUS       "); 
+          lcd.setCursor(0, 3);        
+          lcd.print("      RUNNING!      "); 
+
+          // Out
+          digitalWrite(pinFan, HIGH);
+          digitalWrite(pinHeater, HIGH);
+          digitalWrite(pinLedGreen, HIGH);
+          digitalWrite(pinLedRed, LOW);
+          digitalWrite(pinLedOrange, LOW);
+        }
+
+        // Display - Complete
+        if (dStatus == "2")
+        {
+          lcd.setCursor(0, 0);        
+          lcd.print("                    "); 
+          lcd.setCursor(0, 1);        
+          lcd.print("       STATUS       "); 
+          lcd.setCursor(0, 2);        
+          lcd.print("      COMPLETE      "); 
+          lcd.setCursor(0, 3);        
+          lcd.print("                    "); 
+
+          // Out
+          digitalWrite(pinFan, LOW);
+          digitalWrite(pinHeater, LOW);
+          digitalWrite(pinLedGreen, LOW);
+          digitalWrite(pinLedRed, HIGH);
+          digitalWrite(pinLedOrange, LOW);
+        }
       }
 
-      // Display - In use
-      if (dStatus == "1")
+      // Disconnected
+      else 
       {
-        lcd.setCursor(0, 0);        
-        lcd.print("     " + dTemp + "c    " + dHumi + "%     "); 
-        lcd.setCursor(0, 1);        
-        lcd.print(" " + dTimer + "--" + dTimerMain + " "); 
-        lcd.setCursor(0, 2);        
-        lcd.print("       STATUS       "); 
-        lcd.setCursor(0, 3);        
-        lcd.print("      RUNNING!      "); 
-
-        // Out
-        digitalWrite(pinFan, HIGH);
-        digitalWrite(pinHeater, HIGH);
-        digitalWrite(pinLedGreen, HIGH);
-        digitalWrite(pinLedRed, LOW);
-        digitalWrite(pinLedOrange, LOW);
+          lcd.setCursor(0, 0);        
+          lcd.print("                    "); 
+          lcd.setCursor(0, 1);        
+          lcd.print("        WIFI        "); 
+          lcd.setCursor(0, 2);        
+          lcd.print("     CONNECTING     "); 
+          lcd.setCursor(0, 3);        
+          lcd.print("                    "); 
       }
 
-      // Display - Complete
-      if (dStatus == "2")
+      // Next
+      lastTime = millis();
+    }
+  }
+
+  // MANUAL
+  if (wifiStatus == 0)
+  {
+    // Main Display
+    if (isSetup == 0)
+    {
+      // Btn?
+      if ((millis() - lastTimeBtn) > timerDelay2) 
+      {
+        if (!btnOKVal)
+        {
+          isSetup = 1;
+
+          // Next
+          lastTimeBtn = millis();
+          return;
+        }
+
+        if (!btnLockVal)
+        {
+          if (manualTimer > 0)
+          {
+            return;
+          }
+
+          if (dLock == "0")
+          {
+            dLock = "1";
+            lastTimeBtn = millis();
+            return;
+          }
+
+          if (dLock == "1")
+          {
+            dLock = "0";
+            lastTimeBtn = millis();
+            return;
+          }
+        }
+      }
+
+      // running
+      if (manualTimer > 0)
+      {
+        if ((millis() - lastTimeManualRunning) > timerDelay) 
+        {
+          ConvertTime(manualTimer);
+
+          lcd.setCursor(0, 0);      
+          lcd.print("   " + String(sTemp) + "c  " + String(sHumi) + "%   "); 
+          lcd.setCursor(0, 1);        
+          lcd.print("      " + dTimer + "      "); 
+          lcd.setCursor(0, 2);        
+          lcd.print("       STATUS       "); 
+          lcd.setCursor(0, 3);        
+          lcd.print("      RUNNING!      "); 
+
+          // Out
+          digitalWrite(pinFan, HIGH);
+          digitalWrite(pinHeater, HIGH);
+          digitalWrite(pinLedGreen, HIGH);
+          digitalWrite(pinLedRed, LOW);
+          digitalWrite(pinLedOrange, LOW);
+
+          //
+          manualTimer = manualTimer - 1;
+          lastTimeManualRunning = millis();
+        }
+      }
+
+      // complete
+      else if (manualTimer == 0)
       {
         lcd.setCursor(0, 0);        
         lcd.print("                    "); 
@@ -188,31 +328,142 @@ void loop()
         digitalWrite(pinLedOrange, LOW);
       }
 
-      // Lock?
-      if (dLock == "0")
+      // idle
+      else 
       {
-        digitalWrite(pinLock, LOW);
-      }
-
-      if (dLock == "1")
-      {
-        digitalWrite(pinLock, HIGH);
-      }
-    }
-    else 
-    {
         lcd.setCursor(0, 0);        
-        lcd.print("                    "); 
+        lcd.print("       STATUS       "); 
         lcd.setCursor(0, 1);        
-        lcd.print("        WIFI        "); 
+        lcd.print("        IDLE        "); 
         lcd.setCursor(0, 2);        
-        lcd.print("     CONNECTING     "); 
-        lcd.setCursor(0, 3);        
         lcd.print("                    "); 
+        lcd.setCursor(0, 3);        
+        lcd.print("       Manual       "); 
+
+        // Out
+        digitalWrite(pinFan, LOW);
+        digitalWrite(pinHeater, LOW);
+        digitalWrite(pinLedGreen, LOW);
+        digitalWrite(pinLedRed, LOW);
+        digitalWrite(pinLedOrange, HIGH);
+      }
     }
 
-    lastTime = millis();
+    // Timer Display
+    else
+    {
+      // Btn?
+      if ((millis() - lastTimeBtn) > timerDelay2) 
+      {
+        if (!btnUpVal)
+        {
+          manualTimer = manualTimer + 60;
+
+          // Next
+          lastTimeBtn = millis();
+          return;
+        }
+
+        if (!btnDownVal)
+        {
+          manualTimer = manualTimer - 60;
+          if (manualTimer <= 0)
+          {
+            manualTimer = 0;
+          }
+
+          // Next
+          lastTimeBtn = millis();
+          return;
+        }
+
+        if (!btnOKVal)
+        {
+          isSetup = 0;
+
+          // Next
+          lastTimeBtn = millis();
+          return;
+        }
+      }
+
+      ConvertTime(manualTimer);
+
+      lcd.setCursor(0, 0);        
+      lcd.print("     SET  TIMER     "); 
+      lcd.setCursor(0, 1);        
+      lcd.print("      " + dTimer + "      "); 
+      lcd.setCursor(0, 2);        
+      lcd.print("   UP - Increase    "); 
+      lcd.setCursor(0, 3);        
+      lcd.print("  Down - Decrease   "); 
+    }
   }
+
+  // CHOOSE
+  if (wifiStatus == 2)
+  {
+    lcd.setCursor(0, 0);        
+    lcd.print("       WELCOME      "); 
+    lcd.setCursor(0, 1);        
+    lcd.print("    CHOOSE  MODE    "); 
+    lcd.setCursor(0, 2);        
+    lcd.print("    UP - Manual     "); 
+    lcd.setCursor(0, 3);        
+    lcd.print("    Down - Phone    "); 
+
+    // Btn?
+    if ((millis() - lastTimeBtn) > timerDelay) 
+    {
+      if (!btnUpVal)
+      {
+        wifiStatus = 0;
+
+        // Next
+        lastTimeBtn = millis();
+        return;
+      }
+
+      if (!btnDownVal)
+      {
+        wifiStatus = 1;
+
+        // Next
+        lastTimeBtn = millis();
+        return;
+      }
+    }
+  }
+
+  // Lock?
+  {
+    if (dLock == "0")
+    {
+      digitalWrite(pinLock, LOW);
+    }
+
+    if (dLock == "1")
+    {
+      digitalWrite(pinLock, HIGH);
+    }
+  }
+}
+
+void ConnectWifi()
+{
+  Serial.println("init wifi");
+  // init wifi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  wifiConnected = 1;
 }
 
 // Get Status
@@ -369,4 +620,27 @@ void RequestSetTimer()
   }
 
   http.end();
+}
+
+//
+void ConvertTime(int timeRemaining)
+{
+  if (timeRemaining <= 0)
+  {
+    timeRemaining = 0;
+  }
+
+  int hours = timeRemaining / 3600;
+  int minutes = (timeRemaining % 3600) / 60;
+  int seconds = timeRemaining % 60;
+
+  String sHrs = String(hours, DEC);
+  String sMin = String(minutes, DEC);
+  String sSec = String(seconds, DEC);
+
+  if (hours < 10) sHrs = "0" + sHrs;
+  if (minutes < 10) sMin = "0" + sMin;
+  if (seconds < 10) sSec = "0" + sSec;
+
+  dTimer = sHrs + ":" + sMin + ":" + sSec;
 }
